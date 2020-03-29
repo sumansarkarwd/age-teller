5b3e16268fd17d16859ea0912a5c6683113825d2
Suman <sumansarkarwd@gmail.com>
2020-03-29 14:29:27 +0530

    build

---
+++ precache-manifest.ed778a24c07288c1c01daa41e2d2f6ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44264b7c8373deef3f52511721789684",
+    "url": "/age-teller/index.html"
+  },
+  {
+    "revision": "e856249ca64cd458c5e9",
+    "url": "/age-teller/static/css/main.93f3bef6.chunk.css"
+  },
+  {
+    "revision": "9dd8b7111157f453eaed",
+    "url": "/age-teller/static/js/2.0efd9549.chunk.js"
+  },
+  {
+    "revision": "e856249ca64cd458c5e9",
+    "url": "/age-teller/static/js/main.4555f2ac.chunk.js"
+  },
+  {
+    "revision": "46f1abb606a9d9db42f1",
+    "url": "/age-teller/static/js/runtime-main.465697f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bbcd3e31cc68ec1e6e38d31bc04d1f4d.js"
+  "/age-teller/precache-manifest.ed778a24c07288c1c01daa41e2d2f6ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/age-teller/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
